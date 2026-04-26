a1204dab98172e9c31490c2038e1790b5e18863b
knguyencas <168413274+knguyencas@users.noreply.github.com>
2026-04-26 13:34:22 +0700

    Add profile time picker constants

---
+++ constants/profile/time-picker.constants.ts
@@ -0,0 +1,4 @@
+export const TIME_PICKER_ITEM_HEIGHT = 44;
+export const TIME_PICKER_HOURS = Array.from({ length: 24 }, (_, index) => index);
+export const TIME_PICKER_MINUTES = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+
